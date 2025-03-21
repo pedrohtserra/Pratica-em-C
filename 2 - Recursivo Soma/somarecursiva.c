d32cb2260e64929d3c9a925b6110e0a857e2b060
pedrohtserra <pepeserra2023@gmail.com>
2025-03-20 21:28:43 -0300

    Adicionado Exercicio 2

---
+++ 2 - Recursivo Soma/somarecursiva.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int soma(int n);
+
+int main(void)
+{
+    int numero = 0;
+
+    printf("Numero: ");
+    scanf("%d", &numero);
+
+    printf("Resultado: %d", soma(numero));
+
+
+}
+
+int soma(int n)
+{
+    if (n != 0)
+    {
+        return n + soma(n - 1);
+    }
+    else {
+        return n;
+    }
+}
